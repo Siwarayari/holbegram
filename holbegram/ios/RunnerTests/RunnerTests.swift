f6bda5f61d58e400ce67541710580c89b85201b3
Siwarayari <3787@holbertonstudents.com>
2024-01-08 11:39:23 +0000

    holbegram

---
+++ holbegram/ios/RunnerTests/RunnerTests.swift
@@ -0,0 +1,12 @@
+import Flutter
+import UIKit
+import XCTest
+
+class RunnerTests: XCTestCase {
+
+  func testExample() {
+    // Add code to the Runner application
+    // See https://developer.apple.com/documentation/xctest for more information about using XCTest.
+  }
+
+}
